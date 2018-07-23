d6b9afbbe43c07ad148618a5a4f63478d96d4b6f
shrinivasan <shrinivasan@netcalyx.co>
2018-07-23 13:30:10 +0530

    first commit

---
+++ unzip_recursive.py
@@ -0,0 +1,36 @@
+import glob
+import os
+
+
+indir = input("Enter Source Direcoty (Full Path) :")
+
+outdir = input("Enter Destination Directory to extract the compressed directories :")
+
+	
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+
+
+for filename in glob.iglob( indir + '/**/*tar.gz', recursive=True):
+    print("Extracting " + filename)
+
+    extract_tar = "tar xzf " + filename + " -C " + outdir
+    os.system(extract_tar)
+
+
+
+for filename in glob.iglob( indir + '/**/*tar.bz2', recursive=True):
+    print("Extracting " + filename)
+
+    extract_tar_bz2 = "tar xjf " + filename + " -C " + outdir
+    os.system(extract_tar_bz2)
+
+
+
+for filename in glob.iglob( indir + '/**/*.zip', recursive=True):
+    print("Extracting " + filename)
+
+    extract_zip = "unzip " + filename + " -d " + outdir
+    os.system(extract_zip)
+
+    
